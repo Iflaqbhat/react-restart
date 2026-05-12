7d48b146d475fe8e38658be7083691e01fc1df91
iflaq khurshid <iflaqkhurshid@iflaqs-MacBook-Air.local>
2026-05-12 22:44:00 +0530

    counter app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import React, { useState } from 'react'
+//counter app
+import React from 'react'
 
-const App = () => {
-  const [count, setCount] = useState(0);
+function App() {
+  const [count, setCount] = React.useState(0);
+  function increaseCount(){
+    setCount(count + 1);
+    console.log(count);
+  }
+  function decreaseCount(){
+    if(count > 0){
+    setCount(count - 1);
+    console.log(count);
+  }
+  else {
+    alert("Count cannot be less than 0");
+  }
+  }
+  function reset(){
+    setCount(0);
+    console.log(count);
+  }
   return (
     <div>
-      <button onClick={()=>{
-        setCount(count+1)
-      }}>click me  </button>
-      <h1>{count}</h1>
+      <button onClick={decreaseCount}>-</button>
+      <button onClick={increaseCount}>+</button>
+      <button onClick={reset}>Reset</button>
+      <span>{count}</span>
     </div>
   )
 }
