959384f4b58e346363220d7fb80e703eb2e79dee
iflaq khurshid <ifuubhat72@gmail.com>
2026-05-13 11:06:40 +0530

    map in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,17 @@
-//counter app
 import React from 'react'
 
-function App() {
-  const [count, setCount] = React.useState(0);
-  function increaseCount(){
-    setCount(count + 1);
-    console.log(count);
-  }
-  function decreaseCount(){
-    if(count > 0){
-    setCount(count - 1);
-    console.log(count);
-  }
-  else {
-    alert("Count cannot be less than 0");
-  }
-  }
-  function reset(){
-    setCount(0);
-    console.log(count);
-  }
+const App = () => {
+  const users=[
+ { id: 1, name: "Iflaq" },
+ { id: 2, name: "Ali" }
+]
+
   return (
-    <div>
-      <button onClick={decreaseCount}>-</button>
-      <button onClick={increaseCount}>+</button>
-      <button onClick={reset}>Reset</button>
-      <span>{count}</span>
-    </div>
+    <div>{users.map((user,index)=>{
+      return (
+        <div key={index}>{user.name }</div>
+      )
+    })}</div>
   )
 }
 
